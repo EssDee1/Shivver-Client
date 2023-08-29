98f7620696fbd5635af14021a2e649852f7cc91a
Shivansh Dutta <109021679+EssDee1@users.noreply.github.com>
2023-08-30 01:09:23 +0545

    Removed Never Used Variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
@@ -11,7 +11,6 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
